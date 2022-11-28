91b099b031d7ea08c97efc7cdf10bd3148858ef1
sualval <101261165+sualval@users.noreply.github.com>
2022-11-28 18:03:24 +0400

    add homework Servlet-Containers

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
@@ -9,7 +10,7 @@ import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
-
+@Repository
 // Stub
 public class PostRepository {
     private AtomicLong counter = new AtomicLong(0);
